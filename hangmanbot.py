84d3f01f94723b5e6cc1899a9b55dc6a081cec67
Moritz Hepp <moritz.hepp@gmx.de>
2021-03-23 13:20:59 +0100

    renamed word to phrase

--- hangmanbot.py
+++ hangmanbot.py
@@ -11,15 +11,15 @@ class State:
 
 MAX_GUESSES = 5
 class Running(State):
-    word: str
+    phrase: str
     unveiled: [bool]
     wrong_guesses: int
 
-    def __init__(self, word: str):
-        if not word:
+    def __init__(self, phrase: str):
+        if not phrase:
             raise ValueError("Word has to be a non empty string")
-        self.word = word
-        self.unveiled = [False for _ in range(len(word))]
+        self.phrase = phrase
+        self.unveiled = [False for _ in range(len(phrase))]
         self.wrong_guesses = 0
         self.__solve(lambda char: not char.isalnum())
 
@@ -27,35 +27,35 @@ class Running(State):
         result = ""
         for index, char in enumerate(self.unveiled):
             if char:
-                result += f" {self.word[index]} "
+                result += f" {self.phrase[index]} "
             else:
                 result += " _ "
         return result
     
     def __solve(self, guess) -> bool:
         contained = False
-        for index, char in enumerate(self.word):
+        for index, char in enumerate(self.phrase):
             if guess(char):
                 contained = True
                 self.unveiled[index] = True
         return contained
 
     def guess(self, guess: str, guesser: discord.Member):
-        """Guessing a single character or the whole word"""
+        """Guessing a single character or the whole phrase"""
         if len(guess) == 1:
             contained = self.__solve(lambda char: char.lower() == guess.lower())
             
             if not contained:
                 self.wrong_guesses += 1
-        elif self.word.lower() == guess.lower():
-            return Solved(word=self.word, solver=guesser)
+        elif self.phrase.lower() == guess.lower():
+            return Solved(phrase=self.phrase, solver=guesser)
         else:
             self.wrong_guesses += 1
 
         if self.wrong_guesses >= MAX_GUESSES:
-            return Failed(self.word)
+            return Failed(self.phrase)
         elif all(self.unveiled):
-            return Solved(word=self.word, solver=guesser)
+            return Solved(phrase=self.phrase, solver=guesser)
         else:
             return self
     
@@ -63,24 +63,24 @@ class Running(State):
         return f"Remaining Bad Guesses:\t{MAX_GUESSES - self.wrong_guesses}\n```\n{self.__unveiled()}\n```"
 
 class Solved(State):
-    word: str
+    phrase: str
     solver: discord.Member
 
-    def __init__(self, word: str, solver: discord.Member):
-        self.word = word
+    def __init__(self, phrase: str, solver: discord.Member):
+        self.phrase = phrase
         self.solver = solver
 
     def __str__(self) -> str:
-        return f"__Solved!__ {self.solver.mention} won and guessed the phrase `{self.word}`"
+        return f"__Solved!__ {self.solver.mention} won and guessed the phrase `{self.phrase}`"
 
 class Failed(State):
-    word: str
+    phrase: str
 
-    def __init__(self, word: str):
-        self.word = word
+    def __init__(self, phrase: str):
+        self.phrase = phrase
 
     def __str__(self) -> str:
-        return f"__Failed!__ The word was `{self.word}`"
+        return f"__Failed!__ The phrase was `{self.phrase}`"
 
 class Hangmanbot(discord.Client):
     states = {}
@@ -95,8 +95,8 @@ class Hangmanbot(discord.Client):
         print('Message from {0.author}: {0.content}'.format(message))
 
         if message.content.startswith("!start_hangman"):
-            word = message.content.replace("!start_hangman", "").strip(" |")
-            if len(word) <= 2:
+            phrase = message.content.replace("!start_hangman", "").strip(" |")
+            if len(phrase) <= 2:
                 raise ValueError("Word has to be at least 3 characters long")
             if not isinstance(message.channel, discord.TextChannel) and not isinstance(message.channel, discord.GroupChannel):
                 await message.channel.send("Can only start hangman in text or group channels")
@@ -104,11 +104,11 @@ class Hangmanbot(discord.Client):
             
             await message.delete()
 
-            self.states[message.channel.id] = Running(word)
+            self.states[message.channel.id] = Running(phrase)
             await message.channel.send(f"{self.states.get(message.channel.id)}")
         elif message.content.startswith("!guess"):
             if (message.channel.id not in self.states) and (not isinstance(self.states.get(message.channel.id), Running)):
-                await message.channel.send("No guess running in this channel. Please start with `!start <word>` first")
+                await message.channel.send("No guess running in this channel. Please start with `!start_hangman ||<phrase>||` first")
                 return
 
             guess = message.content.replace("!guess", "").strip()
