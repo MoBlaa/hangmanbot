2eadbfba9e0521c678f18c64e0998481d4019539
moblaa <mo.blaa@pm.me>
2021-04-10 19:47:51 +0200

    added missing error

--- hangmanbot/__main__.py
+++ hangmanbot/__main__.py
@@ -228,6 +228,7 @@ async def __guess(ctx: commands.Context, *, guess: str):
 @__remove.error
 @__cooldown_edit.error
 @__get_cooldown.error
+@__post_state.error
 async def __handle_error(ctx: commands.Context, error):
     if isinstance(error, commands.BotMissingPermissions):
         await ctx.channel.send(
